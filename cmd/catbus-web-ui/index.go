66882ee4f19429fbe7e0087d05591f07b7ff0e84
Ethel Morgan <eth@ethulhu.co.uk>
2020-11-28 14:46:59 +0000

    use templates instead of manual HTML construction

---
+++ cmd/catbus-web-ui/index.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+
+	"go.eth.moe/catbus-web-ui/home"
+)
+
+var (
+	funcs = map[string]interface{}{
+		"controlTmpl": controlTmpl,
+	}
+
+	indexTmpl = template.Must(template.New("index.html").
+			Funcs(funcs).
+			Parse(`<DOCTYPE html>
+<html lang='en'>
+<head>
+  <title>Home</title>
+</head>
+<body>
+  <h1>Home</h1>
+  {{ range .Zones }}
+  <section>
+    <h2>{{ .Name }}</h2>
+    {{ range .Devices }}
+      <section>
+        <h3>{{ .Name }}</h3>
+	<table>
+	{{ range .Controls }}
+	  <tr>
+	    <td>{{ .Name }}</td>
+	    <td>{{ controlTmpl . }}</td>
+	  </tr>
+	{{ end }}
+	</table>
+      </section>
+    {{ end }}
+  </section>
+  {{ end }}
+</body>
+</html>`))
+
+	enumTmpl = template.Must(template.New("enum").Parse(`
+{{ $value := .Value }}
+<select>
+{{ range .Values }}
+  <option {{ if eq $value . }}selected{{ end}}>{{ . }}</option>
+{{ end }}
+</select>`))
+
+	rangeTmpl = template.Must(template.New("range").Parse(
+		"<input type='range' min='{{ .Min }}' max='{{ .Max }}' value='{{ .Value }}'>",
+	))
+
+	toggleTmpl = template.Must(template.New("toggle").Parse(
+		"<input type='checkbox' {{ if .Value }}checked{{ end }}>",
+	))
+)
+
+func controlTmpl(control home.Control) template.HTML {
+	var w bytes.Buffer
+	switch control := control.(type) {
+	case *home.Enum:
+		if err := enumTmpl.Execute(&w, control); err != nil {
+			log.Printf("could not fill template: %v", err)
+		}
+	case *home.Range:
+		if err := rangeTmpl.Execute(&w, control); err != nil {
+			log.Printf("could not fill template: %v", err)
+		}
+	case *home.Toggle:
+		if err := toggleTmpl.Execute(&w, control); err != nil {
+			log.Printf("could not fill template: %v", err)
+		}
+	default:
+		panic("unknown control type")
+	}
+	return template.HTML(w.String())
+}
--- cmd/catbus-web-ui/main.go
+++ cmd/catbus-web-ui/main.go
@@ -15,7 +15,6 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"sort"
 	"sync"
 
 	"github.com/gorilla/mux"
@@ -143,10 +142,6 @@ func main() {
 			w.Write(bytes)
 		})
 
-	// Return the tree of zones/devices/controls under home/{path} as JSON.
-	// For example,
-	// 	GET /home/ => the entire home.
-	// 	GET /home/bedroom => everything under home/bedroom.
 	m.PathPrefix("/home/").
 		Methods("GET").
 		HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -154,60 +149,9 @@ func main() {
 			defer payloadByTopicMu.RUnlock()
 			h := home.OfValuesByTopic(payloadByTopic)
 
-			fmt.Fprintf(w, "<html><head><title>Home</title></title>")
-			fmt.Fprintf(w, "<body><h1>Home</h1>")
-			zones := h.Zones()
-			sort.Slice(zones, func(i, j int) bool {
-				return zones[i].Name() < zones[j].Name()
-			})
-
-			for _, zone := range zones {
-				fmt.Fprintf(w, "<section><h2>%s</h2>", zone.Name())
-				devices := zone.Devices()
-				sort.Slice(devices, func(i, j int) bool {
-					return devices[i].Name() < devices[j].Name()
-				})
-
-				for _, device := range devices {
-					fmt.Fprintf(w, "<section><h3>%s</h3>", device.Name())
-					controls := device.Controls()
-					sort.Slice(controls, func(i, j int) bool {
-						return controls[i].Name() < controls[j].Name()
-					})
-
-					fmt.Fprintf(w, "<table>")
-					for _, control := range controls {
-						fmt.Fprintf(w, "<tr><td>%s</td><td>", control.Name())
-						switch control := control.(type) {
-						case *home.Enum:
-							fmt.Fprintf(w, "<select>")
-							for _, value := range control.Values {
-								if value == control.Value {
-									fmt.Fprintf(w, "<option selected>%s</option>", value)
-								} else {
-									fmt.Fprintf(w, "<option>%s</option>", value)
-								}
-							}
-							fmt.Fprintf(w, "</select>")
-						case *home.Range:
-							fmt.Fprintf(w, "<input type='range' min='%v' max='%v' value='%v'>", control.Min, control.Max, control.Value)
-						case *home.Toggle:
-							if control.Value {
-								fmt.Fprintf(w, "<input type='checkbox' checked>")
-							} else {
-								fmt.Fprintf(w, "<input type='checkbox'>")
-							}
-						default:
-							panic("unknown control type")
-						}
-						fmt.Fprintf(w, "</td></tr>")
-					}
-					fmt.Fprintf(w, "</table>")
-					fmt.Fprintf(w, "</section>")
-				}
-				fmt.Fprintf(w, "</section>")
+			if err := indexTmpl.Execute(w, h); err != nil {
+				log.Printf("could not template: %v", err)
 			}
-			fmt.Fprintf(w, "</body></html>")
 		})
 
 	statikFS, err := fs.New()
